bb5e8efdfaa431395c7059114e41774f23e9e176
Renato Menegasso <renato.menegasso@bidu.com.br>
2015-05-30 18:57:15 -0300

    Implement `yesterday` and `tomorrow` urls

--- app/controllers/date_controller.rb
+++ app/controllers/date_controller.rb
@@ -9,6 +9,10 @@ class DateController < ApplicationController
   def date
     @date ||=  if params.key? :today
       Date.today
+    elsif params.key? :tomorrow
+      Date.tomorrow
+    elsif params.key? :yesterday
+      Date.yesterday
     else
       Date.parse("#{params[:day]} #{params[:month]}", format: :short)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :holiday, path: 'feriado', only: [:show]
 
   get '/dia-:day-de-:month', to: 'date#show', as: :date
+  get '/ontem', to: 'date#show', defaults: { yesterday: true }
+  get '/amanha', to: 'date#show', defaults: { tomorrow: true }
   get '/:slug', to: 'holiday#show'
 end
