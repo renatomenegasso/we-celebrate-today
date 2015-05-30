868dec8111312832170e14554f8befcd81e3442d
Renato Menegasso <renato.menegasso@bidu.com.br>
2015-05-30 11:12:13 -0300

    Application bootstrap

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.1'
+gem 'sqlite3'
+
+gem 'jquery-rails'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+
+group :development, :test do
+  gem 'pry-rails'
+  gem 'pry-nav'
+
+  gem 'web-console', '~> 2.0'
+
+  gem 'spring'
+  gem 'rspec-rails', '~> 3.2'
+end
---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails'
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+# require "action_mailer/railtie"
+require 'action_view/railtie'
+require 'sprockets/railtie'
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module WeCelebrateToday
+  class Application < Rails::Application
+    config.time_zone = 'Brasilia'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
